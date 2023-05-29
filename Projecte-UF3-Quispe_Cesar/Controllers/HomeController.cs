77be2c72402ae4527a6671cd822f741fb1d83228
cdqa26071991 <cdqa26071991mail.com>
2023-05-29 04:20:10 -0500

    Proyecto Api UF3

---
+++ Projecte-UF3-Quispe_Cesar/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_API.Models;
+using Newtonsoft.Json;
+
+namespace Proyecto_API.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        const string apiUrl = "https://biriyani.anoram.com/get";
+        var client = new HttpClient();
+        var response = client.GetAsync(apiUrl).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        
+        Cocina imatgeDeCocina = JsonConvert.DeserializeObject<Cocina>(content);
+    
+
+        return View(imatgeDeCocina);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult receta()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    
+}
---
+++ Projecte-UF3-Quispe_Cesar/Models/cocina.cs
@@ -0,0 +1,6 @@
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Cocina
+    {
+        public string image { get; set; }
+    }
+    
